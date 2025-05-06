e01080c08f0f612a3753912928d6f79754828dee
justvarma <146994690+justvarma@users.noreply.github.com>
2025-05-06 09:21:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,10 +3,28 @@ from config import SQLALCHEMY_DATABASE_URI
 from models import db, ProductStock
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']=SQLALCHEMY_DATABASE_URI
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+db.init_app(app)
+
+@app.route("/stock/add", methords=['POST'])
+def add_stock():
+    data=request.get_json()
+    product_id=data['product_id']
+    quantity=data['quantity']
+
+    stock=ProductStock.query.get(product_id)
+    if stock:
+        stock.quantity+=quantity
+        db.session.commit(stock)
+        return {'message': 'Stock created'}, 200
+    else:
+        stock=ProductStock(product_id=data['product_id'], quantity=data['quantity'])
+        db.session.add(stock)
+        db.session.commit()
+        return {'message': 'Stock created'}, 201
 
 if __name__=='__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
